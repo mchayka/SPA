1229ab4231e04d3469a13226986044d287a417c0
vu <vik.ugrin@gmail.com>
2016-02-29 22:19:28 +0200

    update

--- src/app/components/chats/chat/message/message.controller.js
+++ src/app/components/chats/chat/message/message.controller.js
@@ -13,7 +13,7 @@
                     watching();
                 }
             });
-        } else {
+        } else if($scope.message.status == 1 && $scope.message.sender != $scope.accountInfo.uid && $scope.chat.status == 2) {
             messageService.updateStatus($scope.accountInfo.uid, $scope.chatId, $scope.message);
         }
     }
